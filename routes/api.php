98a74d4695835aafb6019b1d57b7de5a021c1fe7
Rana Bepari <irana.bpr9@gmail.com>
2026-02-25 11:34:16 +0600

    feat: Redirect Login

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->get('/sso/user', function (Request $request) {
+    return $request->user();
+});
