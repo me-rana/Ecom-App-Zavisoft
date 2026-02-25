0660736390fc05c700f4fedbe9c810c205c1fb7a
Rana Bepari <irana.bpr9@gmail.com>
2026-02-25 11:39:16 +0600

    feat: Update

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -187,6 +187,12 @@
             </x-responsive-nav-link>
         </div>
 
+              <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link href="{{ route('go.food') }}" :active="request()->routeIs('go.food')">
+                {{ __('Jump to FoodPanda') }}
+            </x-responsive-nav-link>
+        </div>
+
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="flex items-center px-4">
